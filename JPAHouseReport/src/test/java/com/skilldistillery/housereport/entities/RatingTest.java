3de76c5c9984932facf158a27b369b739e1b4d87
Alex Adkins <alex.t.adkins@gmail.com>
2021-07-17 12:34:42 -0400

    ratingid and rating created

---
+++ JPAHouseReport/src/test/java/com/skilldistillery/housereport/entities/RatingTest.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.housereport.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RatingTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
