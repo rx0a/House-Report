cbfc5ff9b61c9f1963dfc3c6c1f365f6af80f5c6
Alex Adkins <alex.t.adkins@gmail.com>
2021-07-17 12:05:47 -0400

    added Address/Event entities + tests

--- JPAHouseReport/src/main/java/com/skilldistillery/housereport/entities/Address.java
+++ JPAHouseReport/src/main/java/com/skilldistillery/housereport/entities/Address.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.housereport.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,10 +12,58 @@ public class Address {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(name="street")
 	private String street;
+	@Column(name="street2")
 	private String street2;
+	@Column(name="city")
 	private String city;
+	@Column(name="state")
 	private String state;
+	@Column(name="postal_code")
 	private String postalCode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getStreet2() {
+		return street2;
+	}
+	public void setStreet2(String street2) {
+		this.street2 = street2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", street=" + street + ", street2=" + street2 + ", city=" + city + ", state="
+				+ state + ", postalCode=" + postalCode + "]";
+	}
+	
+	
 
 }
