1b52a283d70ecd372757c5585f1fbaea30faaea8
Ray S <rayspace@protonmail.com>
2021-07-17 11:26:33 -0600

    user and comments linked together

--- JPAHouseReport/src/main/java/com/skilldistillery/housereport/entities/User.java
+++ JPAHouseReport/src/main/java/com/skilldistillery/housereport/entities/User.java
@@ -1,10 +1,13 @@
 package com.skilldistillery.housereport.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class User {
@@ -22,79 +25,110 @@ public class User {
 	@Column(name = "last_name")
 	private String lastName;
 	private String email;
+	@OneToMany(mappedBy = "user")
+	private List<Comment> comments;
 
 	public User() {
 	}
+
 
 	public int getId() {
 		return id;
 	}
 
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 
 	public String getUsername() {
 		return username;
 	}
 
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 
 	public String getPassword() {
 		return password;
 	}
 
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 
 	public int getEnabled() {
 		return enabled;
 	}
 
+
 	public void setEnabled(int enabled) {
 		this.enabled = enabled;
 	}
+
 
 	public String getRole() {
 		return role;
 	}
 
+
 	public void setRole(String role) {
 		this.role = role;
 	}
+
 
 	public String getFirstName() {
 		return firstName;
 	}
 
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 
 	public String getLastName() {
 		return lastName;
 	}
 
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 
 	public String getEmail() {
 		return email;
 	}
 
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", password=" + password + ", enabled=" + enabled
-				+ ", role=" + role + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+				+ ", role=" + role + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", comments=" + comments + "]";
 	}
+
 
 	@Override
 	public int hashCode() {
