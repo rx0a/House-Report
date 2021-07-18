75182e3271644613109302f94c5fb60b94a70275
Ray S <rayspace@protonmail.com>
2021-07-18 12:24:17 -0600

    pulling

--- HouseReport/src/main/java/com/skilldistillery/housereport/controllers/LoginController.java
+++ HouseReport/src/main/java/com/skilldistillery/housereport/controllers/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.housereport.data.UserDAO;
 import com.skilldistillery.housereport.entities.User;
@@ -20,9 +21,17 @@ public class LoginController {
 	}
 
 	@RequestMapping(path = { "checkAccount.do" }, params = { "username", "password" }, method = RequestMethod.POST)
-	public String checkAccount(Model model, String username, String password) {
-		User user = userDao.findByUsername(username);
-		
-		return "login";
+	public ModelAndView checkAccount(Model model, String username, String password) {
+		ModelAndView mv = new ModelAndView();
+		if (userDao.checkUsername(username)) {
+			User user = userDao.findByUsername(username);		
+			mv.addObject("user", user);
+			mv.setViewName("home");
+			return mv;
+		}
+		else {
+			mv.setViewName("register");
+			return mv;
+		}
 	}
 }
