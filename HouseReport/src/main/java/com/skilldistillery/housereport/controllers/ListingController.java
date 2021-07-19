d5ea9e0fb52be5a31a44f1009f21c03fb65f1b0d
Caleb Koch <caleb.koch@gmail.com>
2021-07-19 09:41:20 -0600

    accuracyRating field added for Listing

--- HouseReport/src/main/java/com/skilldistillery/housereport/controllers/ListingController.java
+++ HouseReport/src/main/java/com/skilldistillery/housereport/controllers/ListingController.java
@@ -4,15 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.skilldistillery.housereport.data.ListingDAO;
 import com.skilldistillery.housereport.data.UserDAO;
+import com.skilldistillery.housereport.entities.Listing;
 @Controller
 public class ListingController {
 	@Autowired
 	private UserDAO userDao;
+	
+	@Autowired
+	private ListingDAO listingDao;
 
 	@RequestMapping(path = { "showListings.do" })
 	public String showListings(Model model) {
 		return "listing";
+	}
+	
+	@RequestMapping(path = { "showRating.do"})
+	public ModelAndView showRating (Listing listing) {
+		ModelAndView mv = new ModelAndView();
+		int listingID = listing.getId();
+		int listingRating = listingDao.getListingRating(listingID);
+		listing.setAccuracyRating(listingRating);
+		mv.addObject("listing", listing);
+		mv.setViewName("listing");
+		return mv;
 	}
 }
