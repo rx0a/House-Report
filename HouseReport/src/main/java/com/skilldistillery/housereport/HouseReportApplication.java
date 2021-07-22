438203f21c83d8c00c68b03d54737a748f9cb5c9
Ray S <rayspace@protonmail.com>
2021-07-22 14:29:44 -0600

    deployed to ec2

--- HouseReport/src/main/java/com/skilldistillery/housereport/HouseReportApplication.java
+++ HouseReport/src/main/java/com/skilldistillery/housereport/HouseReportApplication.java
@@ -2,10 +2,15 @@ package com.skilldistillery.housereport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HouseReportApplication {
+public class HouseReportApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(HouseReportApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HouseReportApplication.class, args);
